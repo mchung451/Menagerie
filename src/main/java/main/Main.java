91ac853b89d04e677f562e77fc64869fb1b985df
Michael <michaelchung451@gmail.com>
2021-09-26 13:19:58 +0100

    another jacoco attempt

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args){
 
         // This comment was added to remove a code smell!
-        // Testing once again for jacoco
+        // Testing once again for jacoco take 2
+
     }
 }
