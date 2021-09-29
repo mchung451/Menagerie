a9d6141abb0a4703d71597e2159ca799fc063208
Michael <michaelchung451@gmail.com>
2021-09-29 17:08:35 +0100

    adding quality gate

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,5 +3,6 @@ package main;
 public class Main {
     public static void main(String[] args){
         // I'll leave this comment in
+        // I'm adding this for the purpose of quality gate
     }
 }
