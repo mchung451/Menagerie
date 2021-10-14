bdfb8455cefebe4f173c1ff833683b0166c875aa
Michael <michaelchung451@gmail.com>
2021-10-14 15:01:11 +0100

    added google calculator test

---
+++ src/test/java/main/CalcTest.java
@@ -0,0 +1,55 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalcTest {
+
+    @Test
+    //Tests google calculator
+    public void googleCalculator(){
+
+
+
+        //Creating a driver object referencing WebDriver
+        WebDriver driver;
+
+        //Setting the webdriver.chrome.driver property to its executable's location
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\micha\\Desktop\\new chromedriver\\chromedriver.exe");
+
+        //Instantiating driver
+        driver = new ChromeDriver();
+
+        //Set implicit wait of 10 seconds
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Launch google
+        driver.get("https://www.google.com/");
+
+        //Accepting user thingy
+        WebElement userPolicy = driver.findElement(By.id("L2AGLb"));
+        userPolicy.click();
+
+        //Write 2+2 in google textbox
+        driver.findElement(By.xpath("//input [@name=\"q\"]")).sendKeys("2+2");
+        driver.findElement(By.xpath("//input [@name=\"q\"]")).sendKeys(Keys.ENTER);
+
+
+        //Get result from calculator
+        WebElement calculatorTextBox = driver.findElement(By.id("cwos"));
+        String result = calculatorTextBox.getText();
+
+        //Verify that result of 2+2 is 4
+        assertEquals(result, "4");
+    }
+
+}
+
